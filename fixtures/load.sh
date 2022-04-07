17a82e03ab117ad9b505fab3411374a6a5669920
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2022-04-07 09:17:32 +0530

    Add fixtures and tests directories

---
+++ fixtures/load.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Dumping data as fixtures can be done as:
+# python manage.py dumpdata misc.language > fixtures/languages.json
+# where misc is the app name and language is the model name.
+
+# TODO: Load fixtures here.
