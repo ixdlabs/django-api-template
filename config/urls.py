4f412264d45349369667c51476caa5f8dbb17939
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2023-09-16 09:48:41 +0530

    Add elements doc ui

--- config/urls.py
+++ config/urls.py
@@ -6,30 +6,24 @@ from drf_spectacular import views as spec_views
 from rest_framework.routers import DefaultRouter
 
 from config.admin import custom_admin_site
+from config.schema import SpectacularElementsView
 
 spectacular_api_view = spec_views.SpectacularAPIView.as_view()
-spectacular_api_docs_view = spec_views.SpectacularRedocView.as_view(url_name="schema")
-
-urlpatterns = [
-    path("api-auth/", include("apps.api_auth.urls")),
-    path("api-schema/", spectacular_api_view, name="schema"),
-    path("api/docs/", spectacular_api_docs_view, name="redoc"),
-]
+spectacular_api_docs_view = SpectacularElementsView.as_view(url_name="schema")
 
 router = DefaultRouter()
 # TODO: Register API view sets here
 
-urlpatterns += [
-    # TODO: Register API views here
+urlpatterns = [
+    path("api-auth/", include("apps.api_auth.urls")),
     path("api/v1/", include(router.urls)),
-    path("api-schema/", spectacular_api_view, name="schema"),
-    path("api/docs/", spectacular_api_docs_view, name="api_docs"),
-]
-
-urlpatterns += [
-    # TODO: Configure admin site endpoint here
+    # TODO: Register API views here
+    # Admin site URLs
     path("custom-admin/", custom_admin_site.urls),
     path("", RedirectView.as_view(pattern_name="custom_admin:index")),
+    # API documentation URLs
+    path("api-schema/", spectacular_api_view, name="schema"),
+    path("api/docs/", spectacular_api_docs_view, name="api_docs"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
