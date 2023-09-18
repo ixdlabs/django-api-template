16066945e3ad9021b1000fc86b0083d88676f7ed
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2023-09-19 00:48:26 +0530

    Fix authentication check for dashboard page

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 from apps.users.models import User
@@ -6,6 +8,10 @@ from apps.users.models import User
 
 class CustomDashboardView(TemplateView):
     template_name = "admin/index.html"
+
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
